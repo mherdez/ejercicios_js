31426ce10e16dbb0c8c28d91eb12166af5afe368
Herdez <mherdez@users.noreply.github.com>
2023-08-10 15:44:13 +0000

    kardex

---
+++ 19_kardex.js
@@ -0,0 +1,42 @@
+
+/* kardex = [
+  { 
+    nombre:
+    puntos:
+  },
+  { 
+    nombre:
+    puntos:
+  },
+  { 
+    nombre:
+    puntos:
+  },
+] */
+
+console.log('Kardex')
+
+// pedir un conjunto de nombre de materia y sus puntos, hasta que la materia sea "fin"
+
+// const kardex = []
+
+// let nombre, puntos;
+
+// do {
+//     nombre = prompt('Dame el nombre de una materia (fin para terminar)')
+//     puntos = +prompt('Dame los puntos de la materia')
+    
+//     if( nombre !== 'fin' ) {
+//         kardex.push(
+//             {
+//                 "nombre": nombre,
+//                 "puntos": puntos
+//             }
+//          )
+//     }
+// } while( nombre !== 'fin' )
+
+// console.log(kardex)
+
+
+const 
