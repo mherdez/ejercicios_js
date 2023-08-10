d4822c472e4e8147d65d984c56e0d65d816a12d1
Herdez <mherdez@users.noreply.github.com>
2023-08-10 16:04:12 +0000

    19 kardex

--- 19_kardex.js
+++ 19_kardex.js
@@ -25,7 +25,7 @@ console.log('Kardex')
 // do {
 //     nombre = prompt('Dame el nombre de una materia (fin para terminar)')
 //     puntos = +prompt('Dame los puntos de la materia')
-    
+
 //     if( nombre !== 'fin' ) {
 //         kardex.push(
 //             {
@@ -39,4 +39,96 @@ console.log('Kardex')
 // console.log(kardex)
 
 
-const 
+const alumnos = [
+    {
+        "nombre": 'Juan',
+        "apellido": 'Martinez',
+        "matricula": '1238895',
+        "activo": false,
+        "kardex": [
+            {
+                "materia": 'Fundamentos de programacion',
+                "calif": 90,
+                "semestre": 1
+            },
+            {
+                "materia": 'Quimica',
+                "calif": 80,
+                "semestre": 1
+            },
+            {
+                "materia": 'Matematicas 1',
+                "calif": 80,
+                "semestre": 1
+            },
+            {
+                "materia": 'Prog Orientada a Objetos',
+                "calif": 85,
+                "semestre": 2
+            },
+            {
+                "materia": 'Fisica',
+                "calif": 75,
+                "semestre": 2
+            },
+            {
+                "materia": 'Matematicas discretas',
+                "calif": 95,
+                "semestre": 2
+            },
+        ]
+
+    },
+    {
+        "nombre": 'Maria',
+        "apellido": 'Lopez',
+        "matricula": '00998895',
+        "activo": true,
+        "kardex": [
+            {
+                "materia": 'Fundamentos de programacion',
+                "calif": 70,
+                "semestre": 1
+            },
+            {
+                "materia": 'Quimica',
+                "calif": 70,
+                "semestre": 1
+            },
+            {
+                "materia": 'Matematicas 1',
+                "calif": 70,
+                "semestre": 1
+            },
+            {
+                "materia": 'Prog Orientada a Objetos',
+                "calif": 80,
+                "semestre": 2
+            },
+            {
+                "materia": 'Fisica',
+                "calif": 80,
+                "semestre": 2
+            },
+            {
+                "materia": 'Matematicas discretas',
+                "calif": 80,
+                "semestre": 2
+            },
+        ]
+
+    },
+]
+
+alumnos.forEach(alumno => {
+    if (alumno.activo) {
+        console.log('----------------')
+        console.log('Nombre del alumno: ' + alumno.nombre)
+        console.log('----------------')
+        alumno.kardex.forEach(materia => {
+            if (materia.semestre === 1) {
+                console.log(`${materia.materia} - ${materia.calif}`)
+            }
+        })
+    }
+})
